cb10af94d425d5a8958d273adf0d210992336eb1
Maxim Menshikov <maxim.menshikov@interpretica.io>
2025-09-29 21:11:45 +0100

    Link script, entrypoint, extras

---
+++ src/bflat/linkerscripts/extra.c
@@ -0,0 +1,36 @@
+
+void __extenddftf2(void) { }
+void __addtf3(void) { }
+void __netf2(void) { }
+void __multf3(void) { }
+void __fixunstfsi(void) { }
+void __floatunsitf(void) { }
+void __subtf3(void) { }
+void __fixtfsi(void) { }
+void __floatsitf(void) { }
+void __eqtf2(void) { }
+void __divtf3(void) { }
+void __letf2(void) { }
+void __trunctfsf2(void) { }
+void __trunctfdf2(void) { }
+void __getf2(void) { }
+void __floatdidf(void) { }
+void __floatunsisf(void) { }
+void __ltdf2(void) { }
+void __gedf2(void) { }
+void __fixdfsi(void) { }
+void __gtdf2(void) { }
+void __floatsisf(void) { }
+void __floatsidf(void) { }
+void __floatunsidf(void) { }
+extern void __muldf3(void) { }
+extern void __mulsf3(void) { }
+extern void __floatundidf(void) { }
+
+void __divdf3(void) { }
+void __subdf3(void) { }
+void __adddf3(void) { }
+void __extendsftf2(void) { }
+void __eqdf2(void) { }
+void __fixunsdfsi(void) { }
+
